066cbc3c000eed67c4c95a0f65f10097676f3748
Rizki <rizki.imam@kudo.co.id>
2017-05-04 14:53:34 +0700

    add service worker

---
+++ script.js
@@ -0,0 +1,16 @@
+if ('serviceWorker' in navigator && 'PushManager' in window) {
+  console.log('Service Worker and Push are supported');
+
+  navigator.serviceWorker.register('sw.js')
+  .then(function(swReg) {
+    console.log('Service Worker is registered', swReg);
+
+    swRegistration = swReg;
+  })
+  .catch(function(error) {
+    console.error('Service Worker Error', error);
+  });
+} else {
+  console.warn('Push messaging is not supported');
+  pushButton.textContent = 'Push Not Supported';
+}
